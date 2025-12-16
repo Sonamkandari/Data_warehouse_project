a682abf0519d7e708094dacbb26fd71cba6ac94c
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2025-12-16 19:27:26 +0530

    Update ddl_silver.sql

--- Script/silver/ddl_silver.sql
+++ Script/silver/ddl_silver.sql
@@ -1,1 +1,88 @@
+USE DataWarehouse;
+-- creating DDL scripts for silver table
+if object_id('silver.crm_info','U') IS NOT NULL
+drop table silver.crm_cust_info;
+go
+
+CREATE TABLE silver.crm_cust_info(
+   cst_id INT,
+   cst_key NVARCHAR(50),
+   cst_firstname NVARCHAR(50),
+   cst_lastname NVARCHAR(50),
+   cst_marital_status NVARCHAR(50),
+   cst_gndr NVARCHAR(50),
+   cst_create_date DATETIME,
+   -- creating an extra column
+   dwh_create_date DATETIME DEFAULT GETDATE()
+);
+Go
+
+if object_id('silver.crm_prd_info','U') IS NOT NULL
+drop table silver.crm_prd_info;
+go
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    cat_id NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+GO
+
+if object_id('silver.crm_sales_details','U') IS NOT NULL
+drop table silver.crm_sales_details;
+go
+create table silver.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cus_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT,
+ dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+);
+GO
+
+if object_id('silver.erp_cust_az12','U') IS NOT NULL
+drop table silver.erp_cust_az12;
+go
+create table silver.erp_cust_az12(
+cid NVARCHAR(50),
+bdate Datetime,
+gen NVARCHAR(50),
+ dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+if object_id('silver.erp_loc_a101','U')IS NOT NULL
+drop table silver.erp_loc_a101;
+go 
+create table silver.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50),
+ dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+if object_id ('silver.erp_px_cat_g1v2','U') IS NOT NULL
+drop table silver.erp_px_cat_g1v2;
+create table silver.erp_px_cat_g1v2(
+id NVARCHAR (50),
+cat NVARCHAR (50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50),
+ dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+
 
